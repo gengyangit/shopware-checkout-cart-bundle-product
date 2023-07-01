37026ce94d3c5f3bb2c4752ac1295d00521c4037
gengyangit <86095921+gengyangit@users.noreply.github.com>
2023-07-01 18:02:03 +0200

    resolve merge conflict

--- src/Subscriber/CartSubscriber.php
+++ src/Subscriber/CartSubscriber.php
@@ -2,51 +2,6 @@
 
 namespace Yanduu\CheckoutCartBundleProduct\Subscriber;
 
-<<<<<<< HEAD
-use Psr\Log\LoggerInterface;
-use Shopware\Core\Checkout\Cart\SalesChannel\CartService;
-use Shopware\Storefront\Page\Product\ProductPageLoadedEvent;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Shopware\Core\Checkout\Cart\Event\CartChangedEvent;
-use Shopware\Core\Checkout\Cart\Event\AfterLineItemRemovedEvent;
-use Shopware\Core\Checkout\Cart\LineItem\LineItem;
-use Shopware\Core\Checkout\Cart\Event\AfterLineItemAddedEvent;
-use Shopware\Core\Checkout\Cart\Event\AfterLineItemQuantityChangedEvent;
-use Shopware\Core\Content\Product\ProductEntity;
-use Shopware\Core\Framework\Context;
-use Shopware\Core\Framework\DataAbstractionLayer\Event\EntityWrittenEvent;
-use Shopware\Core\Framework\DataAbstractionLayer\Event\EntityLoadedEvent;
-use Shopware\Core\Framework\DataAbstractionLayer\Event\EntityDeletedEvent;
-use Shopware\Core\Framework\DataAbstractionLayer\Event\EntityUpdatedEvent;
-use Shopware\Core\Framework\DataAbstractionLayer\Event\EntityWrittenContainerEvent;
-use Shopware\Core\Framework\DataAbstractionLayer\Event\EntitySearchResultLoadedEvent;
-use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
-use Shopware\Core\Framework\Uuid\Uuid;
-
-class CartSubscriber implements EventSubscriberInterface
-{
-    private $logger;
-
-    protected $productRepository;
-
-    protected $context;
-
-
-    protected $cartService;
-
-    public function __construct(
-        EntityRepositoryInterface $productRepository,
-        CartService $cartService,
-        LoggerInterface $logger
-    ){
-        $this->productRepository = $productRepository;
-
-        $this->logger = $logger;
-
-        $this->cartService = $cartService;
-=======
 use Shopware\Core\Checkout\Cart\Cart;
 use Shopware\Core\Checkout\Cart\Event\CartChangedEvent;
 use Shopware\Core\Checkout\Cart\Event\AfterLineItemRemovedEvent;
@@ -88,7 +43,6 @@ class CartSubscriber implements EventSubscriberInterface
     {
         $this->cartService = $cartService;
         $this->productReader = $productReader;
->>>>>>> master
 
         $this->context = Context::createDefaultContext();
     }
@@ -108,11 +62,8 @@ class CartSubscriber implements EventSubscriberInterface
 
     /**
      * @param \Shopware\Core\Checkout\Cart\Event\CartChangedEvent $event
-<<<<<<< HEAD
-=======
      * 
      * @return void
->>>>>>> master
      */
     public function onLineItemAdded(AfterLineItemAddedEvent $event): void
     {
@@ -124,11 +75,7 @@ class CartSubscriber implements EventSubscriberInterface
 
         foreach ($lineItems as $lineItem) {
             if (!$lineItem->getPayload()) {
-<<<<<<< HEAD
-                $this->updateBundleProductQuantity($lineItem, $event);
-=======
                 $this->updateBundleProduct($lineItem, $event);
->>>>>>> master
 
                 continue;
             }
@@ -137,16 +84,12 @@ class CartSubscriber implements EventSubscriberInterface
         }
     }
 
-<<<<<<< HEAD
-    public function onLineItemQuantityChanged(AfterLineItemQuantityChangedEvent  $event) 
-=======
     /**
      * @param \Shopware\Core\Checkout\Cart\Event\AfterLineItemQuantityChangedEvent AfterLineItemQuantityChangedEvent
      * 
      * @return void
      */
     public function onLineItemQuantityChanged(AfterLineItemQuantityChangedEvent  $event): void 
->>>>>>> master
     {
         $items = $event->getItems();
 
@@ -158,26 +101,6 @@ class CartSubscriber implements EventSubscriberInterface
         foreach ($items as $item) {
             $lineItem = $this->getLineItemById($item['id'], $cart);
 
-<<<<<<< HEAD
-            $customFields = $lineItem->getPayloadValue('customFields');
-
-            if (!$customFields) {
-                return;
-            }
-
-            if (!array_key_exists('custom_sl_artikel_VERSANDKARTON', $customFields) 
-                || !isset($customFields['custom_sl_artikel_VERSANDKARTON'])
-            ) {
-                return;
-            }
-
-            $bundleProduct =  $this->getProductBySku($customFields['custom_sl_artikel_VERSANDKARTON']);
-
-            $lineItem = $this->getLineItemByReferencedId(
-                $bundleProduct->getId(), 
-                $event->getCart()
-            );
-=======
             $bundleProduct = $this->getBundleProductByLineItem($lineItem);
 
             if ($bundleProduct === null) {
@@ -185,7 +108,6 @@ class CartSubscriber implements EventSubscriberInterface
             }
 
             $lineItem = $this->getLineItemByReferencedId($bundleProduct->getId(), $cart);
->>>>>>> master
 
             if ($lineItem === null) {
                 continue;
@@ -195,64 +117,6 @@ class CartSubscriber implements EventSubscriberInterface
             $cart->markModified();
             $this->cartService->recalculate($cart, $event->getSalesChannelContext());
         }
-<<<<<<< HEAD
-
-    }
-
-    /**
-     * 
-     */
-    protected function getLineItemById(string $lineItemId, $cart) 
-    {
-        $lineItems = $cart->getLineItems();
-
-        foreach ($lineItems as $lineItem) {
-            if ($lineItem->getId() !== $lineItemId) {
-                continue;
-            }
-
-            return $lineItem;
-        }
-
-        return null;
-    }
-
-    protected function getLineItemByReferencedId(string $referencedId, $cart) 
-    {
-        $lineItems = $cart->getLineItems();
-
-        foreach ($lineItems as $lineItem) {
-            if ($lineItem->getReferencedId() !== $referencedId) {
-                continue;
-            }
-
-            return $lineItem;
-        }
-
-        return null;
-    }
-
-    /**
-     * 
-     */
-    protected function addBundleProduct(LineItem $lineItem, $event): void 
-    {
-        $cart = $event->getCart();
-        
-        $customFields = $lineItem->getPayloadValue('customFields');
-
-        if (!$customFields) {
-            return;
-        }
-
-        if (!array_key_exists('custom_sl_artikel_VERSANDKARTON', $customFields) 
-            || !isset($customFields['custom_sl_artikel_VERSANDKARTON'])
-        ) {
-            return;
-        }
-
-        $bundleProduct =  $this->getProductBySku($customFields['custom_sl_artikel_VERSANDKARTON']);
-=======
     }
 
     /**
@@ -296,7 +160,6 @@ class CartSubscriber implements EventSubscriberInterface
     protected function addBundleProduct(LineItem $lineItem, AfterLineItemAddedEvent $event): void 
     {
         $bundleProduct = $this->getBundleProductByLineItem($lineItem);
->>>>>>> master
 
         if ($bundleProduct == null) {
             return;
@@ -304,10 +167,7 @@ class CartSubscriber implements EventSubscriberInterface
 
         $bundleProductLineItem = $this->createLineItem($bundleProduct, $lineItem->getQuantity());
 
-<<<<<<< HEAD
-=======
         $cart = $event->getCart();
->>>>>>> master
         $cart->add($bundleProductLineItem);
         $cart->markModified();
 
@@ -315,115 +175,24 @@ class CartSubscriber implements EventSubscriberInterface
     }
 
     /**
-<<<<<<< HEAD
-     * 
-     */
-    protected function updateBundleProductQuantity(LineItem $lineItem, $event): void
-=======
      * @param \Shopware\Core\Checkout\Cart\LineItem\LineItem $lineItem
      * @param \Shopware\Core\Checkout\Cart\Event\AfterLineItemRemovedEvent $event
      * 
      * @return void
      */
     protected function updateBundleProduct(LineItem $lineItem, AfterLineItemAddedEvent $event): void
->>>>>>> master
     {
         if (!$lineItem->getReferencedId()) {
             return;
         }
 
-<<<<<<< HEAD
-        $product =  $this->getProductById($lineItem->getReferencedId());
-        $customFields = $product->getCustomFields();
-
-        if (!array_key_exists('custom_sl_artikel_VERSANDKARTON', $customFields) 
-            || !isset($customFields['custom_sl_artikel_VERSANDKARTON'])
-        ) {
-=======
         $bundleProduct = $this->getBundleProductByProductId($lineItem->getReferencedId());
 
         if ($bundleProduct == null) {
->>>>>>> master
             return;
         }
 
         $cart = $event->getCart();
-<<<<<<< HEAD
-        $bundleProduct =  $this->getProductBySku($customFields['custom_sl_artikel_VERSANDKARTON']);
-        
-        $lineItemBundleProduct = $this->getLineItemByReferencedId($bundleProduct->getId(), $cart);
-
-        if ($lineItemBundleProduct === null) {
-            return;
-        }
-
-        $lineItemProduct = $this->getLineItemByReferencedId($lineItem->getReferencedId(), $cart);
-        $lineItemBundleProduct->setQuantity($lineItemProduct->getQuantity());
-        $cart->markModified();
-        $this->cartService->recalculate($cart, $event->getSalesChannelContext());
-
-    }
-
-     /**
-     * @param \Shopware\Core\Checkout\Cart\Event\AfterLineItemRemovedEvent $event
-     */
-    public function onLineItemRemoved(AfterLineItemRemovedEvent $event): void
-    {
-        $cart = $event->getCart();
-        $lineItems= $event->getLineItems();
-
-        foreach ($lineItems as $lineItem) {
-            $customFields = $lineItem->getPayloadValue('customFields');
-            if (!$customFields) {
-                continue;
-            }
-
-            if (!array_key_exists('custom_sl_artikel_VERSANDKARTON', $customFields)
-                || !isset($customFields['custom_sl_artikel_VERSANDKARTON'])
-            ) {
-                continue;
-            }
-
-            $bundleProduct =  $this->getProductBySku($customFields['custom_sl_artikel_VERSANDKARTON']);
-
-            if (!$bundleProduct) {
-                continue;
-            }
-
-            foreach ($cart->getLineItems() as $index => $lineItem2) {
-                if ($lineItem2->getReferencedId() === $bundleProduct->getId()) {
-                    $cart->remove($index);
-                    $cart->markModified();
-                    $this->cartService->recalculate($cart, $event->getSalesChannelContext());                    
-                }
-            }
-        }
-    }
-
-
-    private function getProductBySku(string $productNumber): ?ProductEntity
-    {
-        $criteria = new Criteria();
-        $criteria->addFilter(new EqualsFilter('productNumber', $productNumber));
-
-        $product = $this->productRepository->search($criteria, $this->context)->first();
-
-        return $product;
-    }
-
-    private function getProductById(string $productId): ?ProductEntity
-    {
-        $product = $this->productRepository
-            ->search(new Criteria([$productId]), $this->context)->first();
-
-        return $product;
-    }
-
-    /**
-     * 
-     */
-    protected function createLineItem($product, int $quantity): LineItem 
-=======
 
         $bundleProductLineItem = $this->getLineItemByReferencedId($bundleProduct->getId(), $cart);
 
@@ -486,7 +255,6 @@ class CartSubscriber implements EventSubscriberInterface
      * @return \Shopware\Core\Checkout\Cart\LineItem\LineItem
      */
     protected function createLineItem(ProductEntity $product, int $quantity): LineItem 
->>>>>>> master
     {
         $lineItem = new LineItem($product->getId(),  LineItem::PRODUCT_LINE_ITEM_TYPE);
 
@@ -498,9 +266,6 @@ class CartSubscriber implements EventSubscriberInterface
             ->setQuantity($quantity);
     }
 
-<<<<<<< HEAD
-}
-=======
     /**
      * @param \Shopware\Core\Checkout\Cart\LineItem\LineItem $lineItem
      * 
@@ -551,4 +316,3 @@ class CartSubscriber implements EventSubscriberInterface
     }
 
 }
->>>>>>> master
